8b2ed66a72f81e1e3fe1566ba9288aa595b0e2ef
Adriel Nazar Santos <99226370+adrielnazar@users.noreply.github.com>
2022-12-09 04:17:58 -0300

    18ª aula, começo do projeto

---
+++ costs/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/pages/layout/Container";
+
+function App() {
+  return (
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exect path="/" element={<Home/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/newproject" element={<NewProject/>}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
